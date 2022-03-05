9c928fe89eeef1a9c9b10e435114e4b76d459036
Hector4like6gorillaz <78667228+hector4like6gorillaz@users.noreply.github.com>
2022-03-05 10:01:25 -0600

    changeee

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import React from "react";
 import styled from "./ejemplo.module.css";
 
 const App = () => {
+  const MyName = "hector balan";
+  const poema =
+    " alkjshdjkalsdklasdklajsdlkajdkl aklsjd alksjdalksd lkad alk daklj alksd alkd jlka dlakdjalks jla lakdj lkajd alkd lka djlkad lakdj lkajdkladkj alk ajdlka jdlak djalkd alkd alkj alkdjal dalkdj lakdjal kdakdlajd lkadj alkjshdjkalsdklasdklajsdlkajdkl aklsjd alksjdalksd lkad alk daklj alksd alkd jlka dlakdjalks jla lakdj lkajd alkd lka djlkad lakdj lkajdkladkj alk ajdlka jdlak djalkd alkd alkj alkdjal dalkdj lakdjal kdakdlajd lkadj alkjshdjkalsdklasdklajsdlkajdkl aklsjd alksjdalksd lkad alk daklj alksd alkd jlka dlakdjalks jla lakdj lkajd alkd lka djlkad lakdj lkajdkladkj alk ajdlka jdlak djalkd alkd alkj alkdjal dalkdj lakdjal kdakdlajd lkadj alkjshdjkalsdklasdklajsdlkajdkl aklsjd alksjdalksd lkad alk daklj alksd alkd jlka dlakdjalks jla lakdj lkajd alkd lka djlkad lakdj lkajdkladkj alk ajdlka jdlak djalkd alkd alkj alkdjal dalkdj lakdjal kdakdlajd lkadj alkjshdjkalsdklasdklajsdlkajdkl aklsjd alksjdalksd lkad alk daklj alksd alkd jlka dlakdjalks jla lakdj lkajd alkd lka djlkad lakdj lkajdkladkj alk ajdlka jdlak djalkd alkd alkj alkdjal dalkdj lakdjal kdakdlajd lkadj";
+  const Saludo = (name) => {
+    console.log("hola", name, "como estas?");
+  };
+
   return (
-    <header className={`${styled.HeaderInicial}    `}>
-      <div>
-        <button className={`${styled.BuscarSearchHead}`}>buscar</button>
-      </div>
+    <>
+      <header className={`${styled.HeaderInicial}    `}>
+        <div>
+          <button className={`${styled.BuscarSearchHead}`}>buscar</button>
+        </div>
+        <div className={`${styled.bordes}`}>
+          <input placeholder="ingrese" />
+        </div>
+        <div>
+          <button className={`${styled.otrohHead}`}>buscar</button>
+        </div>
+        <div>
+          <input placeholder="ingrese letra" />
+        </div>
+        <p className={` ${styled.NombreUsuario}   ${styled.bordes}`}>
+          {MyName}
+        </p>
+        <div className={`${styled.avatar}`}>HB</div>
+      </header>
       <div className={`${styled.bordes}`}>
-        <input placeholder="ingrese" />
+        <h1>Oda a la programacion</h1>
+        <p>{poema}</p>
+        <button onClick={() => Saludo(MyName)}>saludo</button>
       </div>
-      <div>
-        <button className={`${styled.otrohHead}`}>buscar</button>
-      </div>
-      <div>
-        <input placeholder="ingrese letra" />
-      </div>
-      <p className={` ${styled.NombreUsuario}   ${styled.bordes}`}>
-        @Hector.Balan
-      </p>
-      <div className={`${styled.avatar}`} >HB</div>
-    </header>
+    </>
   );
 };
 
