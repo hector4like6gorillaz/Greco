93aeac691a2e1a61c8387b80b776811da89ac77b
Hector4like6gorillaz <78667228+hector4like6gorillaz@users.noreply.github.com>
2022-03-12 09:20:11 -0600

    calculo de area

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "./ejemplo.module.css";
 
 const App = () => {
+  const [Altura, setAltura] = useState(null);
+  const [base, setbase] = useState(null);
+  const [ButtonState, setButtonState] = useState(false);
+
   const MyName = "hector balan";
-  const poema =
-    " alkjshdjkalsdklasdklajsdlkajdkl aklsjd alksjdalksd lkad alk daklj alksd alkd jlka dlakdjalks jla lakdj lkajd alkd lka djlkad lakdj lkajdkladkj alk ajdlka jdlak djalkd alkd alkj alkdjal dalkdj lakdjal kdakdlajd lkadj alkjshdjkalsdklasdklajsdlkajdkl aklsjd alksjdalksd lkad alk daklj alksd alkd jlka dlakdjalks jla lakdj lkajd alkd lka djlkad lakdj lkajdkladkj alk ajdlka jdlak djalkd alkd alkj alkdjal dalkdj lakdjal kdakdlajd lkadj alkjshdjkalsdklasdklajsdlkajdkl aklsjd alksjdalksd lkad alk daklj alksd alkd jlka dlakdjalks jla lakdj lkajd alkd lka djlkad lakdj lkajdkladkj alk ajdlka jdlak djalkd alkd alkj alkdjal dalkdj lakdjal kdakdlajd lkadj alkjshdjkalsdklasdklajsdlkajdkl aklsjd alksjdalksd lkad alk daklj alksd alkd jlka dlakdjalks jla lakdj lkajd alkd lka djlkad lakdj lkajdkladkj alk ajdlka jdlak djalkd alkd alkj alkdjal dalkdj lakdjal kdakdlajd lkadj alkjshdjkalsdklasdklajsdlkajdkl aklsjd alksjdalksd lkad alk daklj alksd alkd jlka dlakdjalks jla lakdj lkajd alkd lka djlkad lakdj lkajdkladkj alk ajdlka jdlak djalkd alkd alkj alkdjal dalkdj lakdjal kdakdlajd lkadj";
+
   const Saludo = (name) => {
     console.log("hola", name, "como estas?");
   };
-
-
-
+  const handleErase = () => {
+    setButtonState(false);
+    setAltura(0);
+    setbase(0);
+  };
   return (
     <>
       <header id={`idEjemplo`} className={`${styled.HeaderInicial}    `}>
@@ -26,27 +31,51 @@ const App = () => {
         <div>
           <input placeholder="ingrese letra" />
         </div>
-        <p className={` ${styled.NombreUsuario} ${styled.bordes}`}>
-          {MyName}
-        </p>
+        <p className={` ${styled.NombreUsuario} ${styled.bordes}`}>{MyName}</p>
         <div className={`${styled.avatar}`}>HB</div>
       </header>
       <div className={` ${styled.scroll} ${styled.bordes}`}>
         <h1>Oda a la programacion</h1>
         <p>hola buenos dias</p>
-        <label>my nombre</label><input type="number" />  <label>my nombre</label>
+        <label>my nombre</label>
+        <input type="number" /> <label>my nombre</label>
         <p>hola buenos dias</p>
         <p>hola buenos dias</p>
-        <label>my nombre</label><input type="number" />  <label>my nombre</label>
+        <label>my nombre</label>
+        <input type="number" /> <label>my nombre</label>
         <p>hola buenos dias</p>
         <p>hola buenos dias</p>
-        <label>my nombre</label><input type="number" />  <label>my nombre</label>
+        <label>my nombre</label>
+        <input type="number" /> <label>my nombre</label>
         <p>hola buenos dias</p>
         <p>hola buenos dias</p>
-        <label>my nombre</label><input type="number" />  <label>my nombre</label>
+        <label>my nombre</label>
+        <input type="number" /> <label>my nombre</label>
         <p>hola buenos dias</p>
         <p>hola buenos dias</p>
-        <button onClick={()=> Saludo(MyName)}>saludo</button>
+        <button onClick={() => Saludo(MyName)}>saludo</button>
+      </div>
+      <div className={``}>
+        <p>
+          ingresa la altura y la base del cuadrilatero para determinar su area
+        </p>
+        <label>ingresa la altura</label>{" "}
+        <input
+          onChange={(e) => setAltura(e.target.value)}
+          value={Altura}
+          type={"number"}
+        />
+        <tr />
+        <label>ingresa la base</label>{" "}
+        <input
+          onChange={(e) => setbase(e.target.value)}
+          value={base}
+          type={"number"}
+        />
+        <tr />
+        <button onClick={() => setButtonState(true)}>Calcular area</button>
+        <button onClick={handleErase}>Borrar</button>
+        {ButtonState && <p>el area de tu cuadrilatero es {base * Altura}</p>}
       </div>
     </>
   );
