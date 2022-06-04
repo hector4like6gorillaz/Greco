955040c7d8d8630ad79f046b2807c4201b702f97
Hector4like6gorillaz <78667228+hector4like6gorillaz@users.noreply.github.com>
2022-06-04 07:46:33 -0500

    add axios and pokemon api

--- src/App.js
+++ src/App.js
@@ -1,88 +1,25 @@
-import React, { useState } from "react";
-import ComponenteFuncional from "./ComponenteFuncional";
+import React, { useEffect } from "react";
 import style from "./card.module.css";
-import NavBarMysite from "./NavBarMysite";
-import OtroEjemplo from "./OtroEjemplo";
+import { envAPI } from "./ApiCalls";
 
 const App = () => {
-  const [Altura, setAltura] = useState(null);
-  const [base, setbase] = useState(null);
-  const [ButtonState, setButtonState] = useState(false);
-  const [porqueUsestate, setPorqueUsestate] = useState("");
-  const MyName = "hector balan";
+  const response = async (page) => envAPI(page);
+  async function loadapi() {
+    const resp = await response(`pokemon/?offset=0&limit=151`);
+    console.log(resp);
+  }
 
-  const datosApi = [
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-    "https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg",
-    "https://i.blogs.es/2e39a5/anniversaryposter2019/840_560.jpeg",
-  ];
-  const Saludo = (name) => {
-    console.log("hola", name, "como estas?");
-  };
-
-  const handleErase = () => {
-    setButtonState(false);
-    setAltura(0);
-    setbase(0);
-  };
-
-  console.log(porqueUsestate);
-  const handleButton = () => {
-    setPorqueUsestate(porqueUsestate + "T");
-  };
+  useEffect(() => {
+    loadapi();
+    return function cleanup() {
+      loadapi();
+    };
+  }, []);
+  
   return (
     <div>
-      <NavBarMysite />
-      <p>hola como estas</p>
-      <p>hola como estas</p>
-      <OtroEjemplo />
-      <hr />
-      <div className={`${style.CardContenedordeCartas}`}>
-        {datosApi.map((item, index) => {
-          let seMuestra = true;
-
-          return (
-            <div key={index}>
-              <ComponenteFuncional
-                name={item}
-                imagen={item}
-                atributo={` ${item} ${index * 5} akjshdsakjhdajksd`}
-                numero={index}
-                show={seMuestra}
-              />
-            </div>
-          );
-        })}
-      </div>
+      <p>Hoy trabajaremos con la api de pokemon</p>
+      <div className={`${style.CardContenedordeCartas}`}></div>
     </div>
   );
 };
